b674ec13e45bfda324c19df346e2a56303506976
ryutaro <ryutaro@icloud.com>
2020-07-07 13:07:58 +0900

    トップページ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "posts/index"
+  get "posts/show"
+  get "posts/new"
+  get "posts/create"
+  get "posts/edit"
+  get "posts/update"
+  get "posts/destroy"
+  root "posts#index"
 end
