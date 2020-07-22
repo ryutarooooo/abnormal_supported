e3e1e371d2f879fd6097fa8273e24db0d52e1e65
ryutaro <ryutaro@icloud.com>
2020-07-22 16:41:28 +0900

    検索機能追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,25 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
 require("@fortawesome/fontawesome-free/js/all")
+
+document.addEventListener("turbolinks:load", () => {
+  searchWord = function () {
+    var searchText = $(this).val().toLowerCase(),
+      targetText;
+
+    $('td.abnormal-place').each(function () {
+      targetText = $(this).text().toLowerCase();
+
+
+      if (targetText.indexOf(searchText) != -1) {
+
+        $(this).parent('tr').removeClass('d-none');
+      } else {
+        $(this).parent('tr').addClass('d-none');
+      }
+    });
+  };
+
+  // searchWordの実行
+  $('#search-text').on('input', searchWord);
+});
